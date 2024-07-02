df36139847da65ae4622853367a3cd028c7defd1
allison-0 <allisonulrich00@gmail.com>
2024-07-02 14:22:06 -0700

    commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def fibo(N):
     a, b = 0, 1 # initialize first two fibo numbers
-
+    count = 0 # counter to keep track of number of terms generated
+    while count < N:
+        yield a # yield current fibo number
+        a, b = b, a + b # update fibo sequence
+        count += 1 # add to counter
 
 
 
